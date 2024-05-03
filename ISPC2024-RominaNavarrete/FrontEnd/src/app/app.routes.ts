bd69bb51b08881cee6f2186285518d067601aa5e
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-03 13:31:39 -0300

    RouterModule!Nav

--- ISPC2024-RominaNavarrete/FrontEnd/src/app/app.routes.ts
+++ ISPC2024-RominaNavarrete/FrontEnd/src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { IniciarSesionComponent } from './pages/auth/iniciar-sesion/iniciar-sesion.component';
-import { RegistroComponent } from './pages/auth/registro/registro.component';
-import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { DestinosComponent } from './pages/destinos/destinos.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { IniciarSesionComponent } from './pages/auth/iniciar-sesion/iniciar-sesion.component';
+import { RegistroComponent } from './pages/auth/registro/registro.component';
 import { Pagina404Component } from './pages/pagina404/pagina404.component';
-import { FooterComponent } from './shared/footer/footer.component';
+
 
 export const routes: Routes = [
     {path: "home", component:HomeComponent},
-    {path:"iniciar-sesion", component:IniciarSesionComponent},
-    {path:"registro", component:RegistroComponent},
-    {path:"nosotros", component: NosotrosComponent},
     {path:"destinos", component: DestinosComponent},
     {path:"contacto", component: ContactoComponent},
-    {path: '**', component: Pagina404Component}
+    {path:"nosotros", component: NosotrosComponent},
+    {path:"iniciar-sesion", component:IniciarSesionComponent},
+    {path:"registro", component:RegistroComponent},
+    {path: '**', component: Pagina404Component},
+    { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
