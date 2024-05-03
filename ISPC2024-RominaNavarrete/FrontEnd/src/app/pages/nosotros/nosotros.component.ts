20b0ba66252774cfe17f7b6f85b6c67902761d05
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-02 21:30:29 -0300

    Estructura desordenada

--- ISPC2024-RominaNavarrete/FrontEnd/src/app/pages/nosotros/nosotros.component.ts
+++ ISPC2024-RominaNavarrete/FrontEnd/src/app/pages/nosotros/nosotros.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [],
+  imports: [NosotrosComponent],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
--- ISPC2024-RominaNavarrete/FrontEnd/src/app/shared/nav/nav.component.ts
+++ ISPC2024-RominaNavarrete/FrontEnd/src/app/shared/nav/nav.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
